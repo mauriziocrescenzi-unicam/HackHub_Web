50930fd6c6c2cf854d98a6b1e228d0e3e8e1269b
mauriziocrescenzi-unicam <maurizio.crescenzi@studenti.unicam.it>
2026-06-15 20:44:14 +0200

    Implementazione navbar home e stile generale

--- hackhub_frontend/src/app/app.ts
+++ hackhub_frontend/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('hackhub_frontend');
+  title = 'HackHub';
 }
