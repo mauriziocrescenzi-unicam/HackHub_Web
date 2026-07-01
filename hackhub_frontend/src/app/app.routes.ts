c58759d387e7414ed9b0232a7d6123e8a5a24394
lucagas-lab <luca.gasparretti@studenti.unicam.it>
2026-07-01 23:49:54 +0200

    modificato app.route
    modificato app.route per poter vedere la home su localhost

--- hackhub_frontend/src/app/app.routes.ts
+++ hackhub_frontend/src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { TeamGuard } from './core/guard/team.guard';
 import { roleGuard } from './core/guard/role.guard';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
